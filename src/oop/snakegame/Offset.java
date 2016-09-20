2b4750228251752a9fee2ebae88c525bf276b008
Aristarch12 <maxim.akulin@gmail.ru>
2016-09-20 09:45:03 +0500

    base classes

---
+++ src/oop/snakegame/Offset.java
@@ -0,0 +1,15 @@
+package oop.snakegame;
+
+public class Offset {
+    private int x;
+    private int y;
+    public  Offset(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public int getX() { return x;}
+    public int getY() { return y;}
+
+
+
+}
