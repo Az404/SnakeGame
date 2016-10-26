c30b5cdf0d55f133890982b4f1e8ae28d335cb11
AZ404 <ztbzak1@rambler.ru>
2016-10-26 13:29:55 +0500

    PlayerActions in Main

--- src/oop/snakegame/playercontrollers/PlayerController.java
+++ src/oop/snakegame/playercontrollers/PlayerController.java
@@ -1,32 +1,23 @@
 package oop.snakegame.playercontrollers;
 
+import oop.snakegame.PlayerAction;
 import oop.snakegame.primitives.Direction;
 import oop.snakegame.Player;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class PlayerController {
     private Player player;
-    private boolean wasCommandReverse = false;
-    private Direction snakeDirection;
+
+    protected List<PlayerAction> actions = new ArrayList<>();
+
     PlayerController(Player player){
         this.player = player;
     }
 
     public void controlPlayer(){
-        if (snakeDirection != null) {
-            player.getSnake().setNextHeadDirection(snakeDirection);
-            snakeDirection = null;
-        }
-        if (wasCommandReverse) {
-            player.getSnake().reverse();
-            wasCommandReverse = false;
-        }
-    }
-
-    public void setSnakeDirection(Direction direction){
-        snakeDirection = direction;
-    }
-    public void reverseSnake() {
-        wasCommandReverse = true;
+        actions.forEach(action -> action.action(player));
+        actions.clear();
     }
 }
